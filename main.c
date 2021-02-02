8a6c3a7c52882c4520142df296a1a98dd64bdd3f
Ayush  Anand <ayush_anand@iitgn.ac.in>
2021-02-02 04:50:18 +0000

    a

--- main.c
+++ main.c
@@ -1,55 +1,21 @@
-#include<stdio.h>
 #include <time.h>
+#include <stdio.h>
 
-#define MAX 500
+long long int factorial(long long int i){
+    if(i<=1)
+    return 1;
+    else{
+    return i*factorial(i-1);}
 
-int multiply(int x, int res[], int res_size);
-
-void factorial(int n)
-{
-    int res[MAX];
-
-    res[0] = 1;
-    int res_size = 1;
-
-    for (int x=2; x<=n; x++)
-    {
-        res_size = multiply(x, res, res_size);
-    }
-
-    
-    // for (int i=res_size-1; i>=0; i--)
-    //     printf("%d",res[i]);
 }
 
-int multiply(int x, int res[], int res_size)
-{
-    int carry = 0;
-
-    for (int i=0; i<res_size; i++)
-    {
-        int prod = res[i] * x + carry;
-        res[i] = prod % 10;  
-        carry  = prod/10;    
-    }
-
-    while (carry)
-    {
-        res[res_size] = carry%10;
-        carry = carry/10;
-        res_size++;
-    }
-    return res_size;
-}
-
-int main()
-{
-    struct timespec before;
+int main(){
+  struct timespec before;
   
   clock_gettime(CLOCK_REALTIME,&before);
-    factorial(100);
-    struct timespec after;
+	long long int fact=factorial(100);
+  struct timespec after;
   clock_gettime(CLOCK_REALTIME,&after);
-  printf("%ld sec, %ld nanoseconds elapsed",after.tv_sec-before.tv_sec,after.tv_nsec-before.tv_nsec);
-    return 0;
+	printf("%ld sec, %ld nanoseconds elapsed",after.tv_sec-before.tv_sec,after.tv_nsec-before.tv_nsec);
+	return 0;
 }
